f4f8f84a937c800b78df5d48d2aa0b6d8ff464d4
Carlos Sandoval <c.sandoval@globant.com>
2024-09-13 00:53:54 -0500

    feat(resource): add resource request implementation

---
+++ src/test/java/com/globant/models/requests/ResourcesRequest.java
@@ -0,0 +1,36 @@
+package com.globant.models.requests;
+
+import com.globant.models.common.Resource;
+import com.globant.models.headers.HeadersBuilder;
+import com.globant.utils.baseRequest.BaseRequest;
+import com.globant.utils.constants.Constants;
+import io.restassured.response.Response;
+
+public class ResourcesRequest extends BaseRequest {
+    private String endpoint;
+
+    public Response getResources() {
+        endpoint = String.format(Constants.API_URL, Constants.RESOURCES_ENDPOINT);
+        return requestGet(endpoint, HeadersBuilder.getBaseHeaders());
+    }
+
+    public Response getResource(String resourceId) {
+        endpoint = String.format(Constants.API_URL_WITH_PARAM, Constants.RESOURCES_ENDPOINT, resourceId);
+        return requestGet(endpoint, HeadersBuilder.getBaseHeaders());
+    }
+
+    public Response createResource(Resource resource) {
+        endpoint = String.format(Constants.API_URL, Constants.RESOURCES_ENDPOINT);
+        return requestPost(endpoint, HeadersBuilder.getBaseHeaders(), resource);
+    }
+
+    public Response updateResource(String resourceId, Resource resource) {
+        endpoint = String.format(Constants.API_URL_WITH_PARAM, Constants.RESOURCES_ENDPOINT, resourceId);
+        return requestPut(endpoint, HeadersBuilder.getBaseHeaders(), resource);
+    }
+
+    public Response deleteResource(String resourceId) {
+        endpoint = String.format(Constants.API_URL_WITH_PARAM, Constants.RESOURCES_ENDPOINT, resourceId);
+        return requestDelete(endpoint, HeadersBuilder.getBaseHeaders());
+    }
+}
