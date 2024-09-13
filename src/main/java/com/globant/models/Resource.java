9c219f63467682265de035c79fd5b6d5b9eae840
Carlos Sandoval <c.sandoval@globant.com>
2024-09-12 23:29:45 -0500

    feat: add base models

---
+++ src/main/java/com/globant/models/Resource.java
@@ -0,0 +1,21 @@
+package com.globant.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resource {
+    private String name;
+    private String tradeMark;
+    private int stock;
+    private double price;
+    private String description;
+    private String tags;
+    private boolean active;
+    private String id;
+}
